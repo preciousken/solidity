c135dbe471c4718e4ab52395267aac7a32f88221
preciousken <pkehinde27@gmail.com>
2023-06-04 00:58:12 +0100

    pushed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
+require("./task/block-number")
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
